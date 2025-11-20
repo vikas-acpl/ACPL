356c76f4df06f44b3d8d94875f7a56ca3b394b1c
Vikas Kumar Verma <vikas.verma@acpl.com>
2025-11-20 12:29:38 +0530

    Add lazy loading

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Spinner.module.css';
+
+const Spinner = () => (
+  <div className="spinner">
+    <div className="spinner-circle"></div>
+  </div>
+);
+
+export default Spinner;
